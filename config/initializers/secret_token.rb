1a5987c4cf0f37db8d5b23ee55cbfafebf410ab0
Bonghyun Kim <bkim515@gmail.com>
2013-01-29 22:57:53 -0800

    R

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bonghyun::Application.config.secret_token = '84611cdf42fdff234db8ab48ab289981eeb8c1fd97d0270c1ce507f29499d37f6f73baac0755c473274832aed04f014ac2f3efe08b276df153d59b935458b6b5'
